29473f0ef59518ec7337c0a456ca3e450f0b0e35
Adrian Fernandez <adrian.f@opendeusto.es>
2024-10-29 13:44:17 +0100

    add: listaPropietarios view

--- Zona_DRS_APP/views.py
+++ Zona_DRS_APP/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from .models import Propietario, Mecanico, Vehiculo, Reparacion
 
 def index(request):
     return HttpResponse('primera vista')
+
+
+def listaPropietarios(request):
+    propietarios = Propietario.objects.order_by('nombre')
+    nombre_dni_propietarios = ', '.join([propietario.nombre + ' (' + propietario.dni + ')' for propietario in propietarios])
+    return HttpResponse(nombre_dni_propietarios)
