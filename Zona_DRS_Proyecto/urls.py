1ae8b834dcca92df3068f30f7d2a3556650a08d6
Adrian Fernandez <adrian.f@opendeusto.es>
2024-10-31 21:38:24 +0100

    fix: visual text of reparations

--- Zona_DRS_Proyecto/urls.py
+++ Zona_DRS_Proyecto/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Zona-DRS/', include('Zona_DRS_APP.urls')),
+    path('ZonaDRS/', include('Zona_DRS_APP.urls')),
 ]
