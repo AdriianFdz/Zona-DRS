47d30bef9c4b9e8908a166e299396071ae40d381
Unai <unai.egusquizadel@opendeusto.es>
2024-11-14 09:06:21 +0100

    Vistas basadas en clases

--- Zona_DRS_APP/urls.py
+++ Zona_DRS_APP/urls.py
@@ -1,14 +1,34 @@
 from django.urls import path
 from . import views
-
+from .views import (
+    IndexView, 
+    ListaPropietariosView, DetallePropietarioView, 
+    ListaMecanicosView, DetalleMecanicoView,
+    ListaVehiculosView, DetalleVehiculoView,
+    ListaReparacionesView, DetalleReparacionView
+) 
 urlpatterns = [
-            path('', views.index, name='index'),
-            path('listaPropietarios/', views.listaPropietarios, name='listaPropietarios'),
-            path('propietario/<str:dni>', views.detallePropietario, name='detallePropietario'),
-            path('listaMecanicos/', views.listaMecanicos, name='listaMecanicos'),
-            path('mecanico/<str:num_ss>', views.detalleMecanico, name='detalleMecanico'),
-            path('listaVehiculos/', views.listaVehiculos, name='listaVehiculos'),
-            path('vehiculo/<str:matricula>', views.detalleVehiculo, name='detalleVehiculo'),
-            path('listaReparaciones/', views.listaReparaciones, name='listaReparaciones'),
-            path('reparacion/<int:id>', views.detalleReparacion, name='detalleReparacion'),
+        
+    # Vista de inicio
+    path('', IndexView.as_view(), name='index'),
+    
+    # Propietarios
+    path('listaPropietarios/', ListaPropietariosView.as_view(), name='listaPropietarios'),
+    path('propietario/<str:pk>/', DetallePropietarioView.as_view(), name='detallePropietario'),
+    
+    # Mecánicos
+    path('listaMecanicos/', ListaMecanicosView.as_view(), name='listaMecanicos'),
+    path('mecanico/<str:pk>/', DetalleMecanicoView.as_view(), name='detalleMecanico'),
+    
+    # Vehículos
+    path('listaVehiculos/', ListaVehiculosView.as_view(), name='listaVehiculos'),
+    path('vehiculo/<str:pk>/', DetalleVehiculoView.as_view(), name='detalleVehiculo'),
+    
+    # Reparaciones
+    path('listaReparaciones/', ListaReparacionesView.as_view(), name='listaReparaciones'),
+    path('reparacion/<int:pk>/', DetalleReparacionView.as_view(), name='detalleVeparacion'),
+    
+
+          
+      
 ]
--- Zona_DRS_APP/views.py
+++ Zona_DRS_APP/views.py
@@ -1,53 +1,98 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Propietario, Mecanico, Vehiculo, Reparacion
+from django.views.generic import ListView, DetailView
+from django.views import View  
 
+'''
 def index(request):
-    return  render(request, 'index.html')
+    return render(request, 'index.html')
+'''
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
 
 
-def listaPropietarios(request):
+# Vistas basadas en clases
+
+class ListaPropietariosView(ListView):
+    model = Propietario
+    template_name = 'listaPropietarios.html'
+    context_object_name = 'propietarios'
+    queryset = Propietario.objects.order_by('nombre')
+    '''
     propietarios = Propietario.objects.order_by('nombre')
     nombre_dni_propietarios = ', '.join([propietario.nombre + ' (' + propietario.dni + ')' for propietario in propietarios])
     return render(request, 'listaPropietarios.html', {'propietarios': propietarios})
-
-def detallePropietario(request, dni):
+    '''
+class DetallePropietarioView(DetailView):
+    model = Propietario
+    template_name = 'detallePropietario.html'
+    context_object_name = 'propietario'
+    '''
         propietario = Propietario.objects.get(dni=dni)
         vehiculos = Vehiculo.objects.filter(propietario=propietario).order_by('matricula')
         return render(request, 'detallePropietario.html', {'propietario': propietario, 'vehiculos': vehiculos});
-
-def listaMecanicos(request):
+    '''
+class ListaMecanicosView(ListView):
+    model = Mecanico
+    template_name = 'listaMecanicos.html'
+    context_object_name = 'mecanicos'
+    queryset = Mecanico.objects.order_by('nombre')
+    '''
     mecanicos = Mecanico.objects.order_by('nombre')
     return render(request, 'listaMecanicos.html', {'mecanicos': mecanicos})
-
-def detalleMecanico(request, num_ss):
+    '''
+class DetalleMecanicoView(DetailView):
+    model = Mecanico
+    template_name = 'detalleMecanico.html'
+    context_object_name = 'mecanico'
+    '''
     try:
         mecanico = Mecanico.objects.get(num_ss=num_ss)
         reparaciones = Reparacion.objects.filter(mecanico=mecanico).order_by('id')
         return render(request, 'detalleMecanico.html', {'mecanico': mecanico, 'reparaciones': reparaciones})
     except Mecanico.DoesNotExist:
         return HttpResponseNotFound('No existe el mecánico con el número de la Seguridad Social proporcionado.')
-
-def listaVehiculos(request):
+    '''
+class ListaVehiculosView(ListView):
+    model = Vehiculo
+    template_name = 'listaVehiculos.html'
+    context_object_name = 'vehiculos'
+    queryset = Vehiculo.objects.order_by('matricula')
+    '''
     vehiculos = Vehiculo.objects.order_by("matricula")
     return render(request, 'listaVehiculos.html', {'vehiculos': vehiculos})
-
-def detalleVehiculo(request, matricula):
+    '''
+class DetalleVehiculoView(DetailView):
+    model = Vehiculo
+    template_name = 'detalleVehiculo.html'
+    context_object_name = 'vehiculo'
+    '''
     try:
         vehiculo = Vehiculo.objects.get(matricula=matricula)
         reparaciones = Reparacion.objects.filter(vehiculo=vehiculo).order_by('id')
         return render(request, 'detalleVehiculo.html', {'vehiculo': vehiculo, 'reparaciones': reparaciones})
     except Vehiculo.DoesNotExist:
         return HttpResponseNotFound('No existe el vehículo con la matrícula proporcionada.')
-
-def listaReparaciones(request):
+    '''
+class ListaReparacionesView(ListView):
+    model = Reparacion
+    template_name = 'listaReparaciones.html'
+    context_object_name = 'reparaciones'
+    queryset = Reparacion.objects.order_by('id')
+    '''
     reparaciones = Reparacion.objects.order_by('id')
     return render(request, 'listaReparaciones.html', {'reparaciones': reparaciones})
-
-def detalleReparacion(request, id):
+    '''
+class DetalleReparacionView(DetailView):
+    model = Reparacion
+    template_name = 'detalleReparacion.html'
+    context_object_name = 'reparacion'
+    '''
     try:
         reparacion = Reparacion.objects.get(id=id)
         return render(request, 'detalleReparacion.html', {'reparacion': reparacion})
     except Reparacion.DoesNotExist:
         return HttpResponseNotFound('No existe la reparación con el identificador proporcionado.')
-        
+    '''
